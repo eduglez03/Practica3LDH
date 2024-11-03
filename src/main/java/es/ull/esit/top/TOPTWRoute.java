9bdda1e34bb7b22b1d012d0ad25573c8f7b1ef3a
eglezgut03 <alu0101461588@ull.edu.es>
2024-11-03 09:23:54 +0000

    Comentada TOPTWRoute

--- src/main/java/es/ull/esit/top/TOPTWRoute.java
+++ src/main/java/es/ull/esit/top/TOPTWRoute.java
@@ -1,40 +1,70 @@
-package es.ull.esit.top;
+package top;
 
 public class TOPTWRoute {
     int predecessor;
     int succesor;
     int id;
-    
+
     TOPTWRoute() {
-        
+
     }
-    
+
+    /**
+     * Constructor de la clase TOPTWRoute.
+     * @param pre Predecesor de la ruta.
+     * @param succ Sucesor de la ruta.
+     * @param id Identificador de la ruta.
+     */
     TOPTWRoute(int pre, int succ, int id) {
         this.predecessor = pre;
         this.succesor = succ;
         this.id = id;
     }
-    
+
+    /**
+     * Método que devuelve el predecesor de la ruta.
+     * @return Predecesor de la ruta.
+     */
     public int getPredeccesor() {
         return this.predecessor;
     }
-    
+
+    /**
+     * Método que devuelve el sucesor de la ruta.
+     * @return Sucesor de la ruta.
+     */
     public int getSuccesor() {
         return this.succesor;
     }
-    
+
+    /**
+     * Método que devuelve el identificador de la ruta.
+     * @return Identificador de la ruta.
+     */
     public int getId() {
         return this.id;
     }
-    
+
+    /**
+     * Método que establece el predecesor de la ruta.
+     * @param pre Predecesor de la ruta.
+     */
     public void setPredeccesor(int pre) {
         this.predecessor = pre;
     }
-    
+
+    /**
+     * Método que establece el sucesor de la ruta.
+     * @param suc Sucesor de la ruta.
+     */
     public void setSuccesor(int suc) {
         this.succesor = suc;
     }
-    
+
+    /**
+     * Método que establece el identificador de la ruta.
+     * @param id Identificador de la ruta.
+     */
     public void setId(int id) {
         this.id = id;
     }
